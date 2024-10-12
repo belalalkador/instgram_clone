ac57e740acfbb8ac11972103782963a2f3e38fb0
belalalkador <belal.alkador1@gmail.com>
2024-10-12 15:19:09 +0300

    Update server.js

--- API/server.js
+++ API/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin:"*",
     credentials: true,
   })
 );
